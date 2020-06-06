ac9a90ac43c06ae668ceb118e8da9b1f20b5a308
bob hall <myk_quayce@hotmail.com>
2020-06-06 22:44:21 +0100

    fixing version numbers

--- teamcity.sh
+++ teamcity.sh
@@ -9,7 +9,7 @@ git clone https://github.com/JetBrains/teamcity-docker-agent.git
 pushd ./teamcity-docker-agent/
 
 sed --expression='s/\bFROM teamcity-minimal-agent:latest\b/FROM jetbrains\/teamcity-minimal-agent:latest/g' --in-place ./ubuntu/Dockerfile
-sed --expression='s/\bDOTNET_SDK_VERSION=2\.2\.103/DOTNET_SDK_VERSION=3.0.100/g' --in-place ./ubuntu/Dockerfile
+sed --expression='s/\bDOTNET_SDK_VERSION=3\.1\.100\b/DOTNET_SDK_VERSION=5.0.100-preview.4.20258.7/g' --in-place ./ubuntu/Dockerfile
 
 docker build --file ./ubuntu/Dockerfile --tag eassbhhtgu/teamcity-net5-agent:latest .
 
