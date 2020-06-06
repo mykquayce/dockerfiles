4d2436355b411869432bd55f870f01425a01b356
bob hall <myk_quayce@hotmail.com>
2020-06-06 22:44:43 +0100

    comments

--- teamcity.sh
+++ teamcity.sh
@@ -1,18 +1,27 @@
 #! /bin/bash
 
+# cleanup
 if [ -d "teamcity-docker-agent" ]; then
   rm --force --recursive ./teamcity-docker-agent/
 fi
 
+# clone the agent repo
 git clone https://github.com/JetBrains/teamcity-docker-agent.git
 
+#
 pushd ./teamcity-docker-agent/
 
+# modify ./ubuntu/Dockerfile
+# fully-qualify "FROM teamcity-minimal-agent:latest" on line 1
 sed --expression='s/\bFROM teamcity-minimal-agent:latest\b/FROM jetbrains\/teamcity-minimal-agent:latest/g' --in-place ./ubuntu/Dockerfile
+# change the sdk version
 sed --expression='s/\bDOTNET_SDK_VERSION=3\.1\.100\b/DOTNET_SDK_VERSION=5.0.100-preview.4.20258.7/g' --in-place ./ubuntu/Dockerfile
 
+# build the image
 docker build --file ./ubuntu/Dockerfile --tag eassbhhtgu/teamcity-net5-agent:latest .
 
+#
 popd
 
+# cleanup
 rm --force --recursive ./teamcity-docker-agent/
