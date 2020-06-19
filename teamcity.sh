acaf5801a71a9643212dc0b7cfdc6d3b12b173a0
bob hall <myk_quayce@hotmail.com>
2020-06-19 23:27:03 +0100

    DOTNET_SDK_VERSION=5.0.100-preview.5.20279.10

--- teamcity.sh
+++ teamcity.sh
@@ -15,7 +15,7 @@ pushd ./teamcity-docker-agent/
 # fully-qualify "FROM teamcity-minimal-agent:latest" on line 1
 sed --expression='s/\bFROM teamcity-minimal-agent:latest\b/FROM jetbrains\/teamcity-minimal-agent:latest/g' --in-place ./ubuntu/Dockerfile
 # change the sdk version
-sed --expression='s/\bDOTNET_SDK_VERSION=3\.1\.100\b/DOTNET_SDK_VERSION=5.0.100-preview.4.20258.7/g' --in-place ./ubuntu/Dockerfile
+sed --expression='s/\bDOTNET_SDK_VERSION=3\.1\.100\b/DOTNET_SDK_VERSION=5.0.100-preview.5.20279.10/g' --in-place ./ubuntu/Dockerfile
 
 # build the image
 docker build --file ./ubuntu/Dockerfile --tag eassbhhtgu/teamcity-net5-agent:latest .
