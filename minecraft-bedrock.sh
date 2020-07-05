3ee0a157a58bea48acfa20e1adaea5a43251623c
bob hall <myk_quayce@hotmail.com>
2020-07-05 02:49:31 +0100

    minecraft-bedrock

--- minecraft-bedrock.sh
+++ minecraft-bedrock.sh
@@ -2,7 +2,43 @@
 
 
 # get the name from the filename (minus extension)
-Name=$(basename --suffix .sh $0)
+Name=$(echo -n $0 | xargs basename --suffix .sh)
+
+
+if [ ! -d ./$Name ];
+then
+	mkdir ./$Name
+fi
+
+
+# get the webpage
+html=$(curl --url https://www.minecraft.net/en-us/download/server/bedrock/)
+
+
+# scrape
+uri=$(echo -n $html | \
+	grep --ignore-case \
+		--only-matching \
+		--perl-regex "https:\/\/minecraft\.azureedge\.net\/bin-linux\/bedrock-server-\d+\.\d+\.\d+\.\d+\.zip")
+
+
+# extract filename
+file_name=$(echo -n $uri | grep --ignore-case --only-matching --perl-regex "bedrock-server-\d+\.\d+\.\d+\.\d+.zip")
+
+
+# if it exists in the archive...
+if [ -f ./$Name/$file_name ];
+then
+	exit 0
+fi
+
+
+# download to the archive directory
+curl --output ./$Name/$file_name --url $uri
+
+
+# copy the latest, to be picked up by the Dockerfile
+cp --update $(ls -1 -t ./$Name/$file_name) ./bedrock-server.zip
 
 
 # search the Dockerfile for images, then "pull" them
@@ -27,3 +63,7 @@ else
 	# update it
 	docker service ls | tail --line +2 | findstr $Name | awk '{system("docker service update --image " $5 " " $2)}'
 fi
+
+
+# push
+docker push eassbhhtgu/minecraft-bedrock:latest
