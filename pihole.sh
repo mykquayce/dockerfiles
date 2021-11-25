479a4387c0e5f652de1d04ec2fe9a6de8b8e2dea
bob h <mail@bob.codes>
2021-11-25 13:11:16 +0000

    improving pihole build script
    ...so it only runs when the base image (i.e., pihole/pihole:latest) has been updated

--- pihole.sh
+++ pihole.sh
@@ -1,23 +1,15 @@
 #! /bin/bash
 
 # pull the latest base image
-docker pull pihole/pihole:latest
+docker pull pihole/pihole:latest | \
+	grep --ignore-case 'Status: Downloaded newer image for'
+
+# if already up to date
+if [ $? -ne 0 ]; then echo everything up to date; exit 0; fi
 
 # build image
 docker build --tag eassbhhtgu/pihole:latest --file ./pihole-dockerfile .
+if [ $? -ne 0 ]; then exit 1; fi
 
 # push image to dockerhub
 docker push eassbhhtgu/pihole:latest
-
-# is the service already running?
-docker service ls --format '{{.Image}}' | grep eassbhhtgu/pihole:latest
-
-if [ $? -ne 0 ]; then
-	# create it
-	docker stack deploy --compose-file ./pihole-docker-compose.yml pihole
-else
-	# update it
-	docker service ls --format '{{.Image}} {{.Name}}' | \
-		findstr eassbhhtgu/pihole:latest | \
-		awk '{system("docker service update --image " $1 " " $2)}'
-fi
